c2f0486fcca44786a64d2db6a6ba4467243c5351
notariq <hauzanbakri@example.com>
2025-04-19 18:16:06 +0700

    update vercel.json and index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -41,19 +41,21 @@ app.use('/api/song', songRoutes);
 app.use('/api/album', albumRoutes);
 
 
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
+});
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
   });
-}
+  
+  module.exports = app;
+} else {
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+    });
+  }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  connectDB();
-});
