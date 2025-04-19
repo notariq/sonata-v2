87dc9a632eb29f4a717608aa91e9a264d6438613
notariq <hauzanbakri@example.com>
2025-04-19 18:02:10 +0700

    add vercel.json

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,5 +2,4 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
 	baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api",
-	//baseURL: "http://localhost:4000/api",
 });
